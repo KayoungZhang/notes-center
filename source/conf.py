796e99e730d46b629dff253b0afb2c5c5f6cadfd
KayoungZhang <51082923+KayoungZhang@users.noreply.github.com>
2021-05-26 15:35:14 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -22,9 +22,8 @@ copyright = '2021, Kayoung'
 author = 'kayoung'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0'
-# The short X.Y version.
-version = '1.0.0'
+release = '1.0.0'
+
 
 # -- General configuration ---------------------------------------------------
 
@@ -68,4 +67,3 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_search_language = u'zh_CN'
